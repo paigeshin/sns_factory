aec8da6653ff9a4bea66619a280feb49733a042f
paigeshin <paigeshin1991@gmail.com>
2023-11-13 00:52:51 +0900

    complete

--- sources/downloaders/youtubeDownloader.js
+++ sources/downloaders/youtubeDownloader.js
@@ -1,72 +1,182 @@
-const fs = require("fs");
-const ytdl = require("ytdl-core");
-const path = require("path");
+/**
+ * Reencode audio & video without creating files first
+ *
+ * Requirements: ffmpeg, ether via a manual installation or via ffmpeg-static
+ *
+ * If you need more complex features like an output-stream you can check the older, more complex example:
+ * https://github.com/fent/node-ytdl-core/blob/cc6720f9387088d6253acc71c8a49000544d4d2a/example/ffmpeg.js
+ */
 
-async function ensureDirectoryExists(filePath) {
-  const dir = path.dirname(filePath);
-  try {
-    await fs.promises.access(dir, fs.constants.F_OK);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      console.log(`Creating directory: ${dir}`);
-      await fs.promises.mkdir(dir, { recursive: true });
-    } else {
-      throw error;
+// Buildin with nodejs
+const cp = require("child_process");
+const readline = require("readline");
+// External modules
+const ytdl = require("ytdl-core");
+const ffmpeg = require("ffmpeg-static");
+
+function downloadVideo(url) {
+  // Global constants
+  const ref = url;
+  const tracker = {
+    start: Date.now(),
+    audio: { downloaded: 0, total: Infinity },
+    video: { downloaded: 0, total: Infinity },
+    merged: { frame: 0, speed: "0x", fps: 0 },
+  };
+
+  // Get audio and video streams
+  const audio = ytdl(ref, { quality: "highestaudio" }).on(
+    "progress",
+    (_, downloaded, total) => {
+      tracker.audio = { downloaded, total };
     }
-  }
+  );
+  const video = ytdl(ref, { quality: "highestvideo" }).on(
+    "progress",
+    (_, downloaded, total) => {
+      tracker.video = { downloaded, total };
+    }
+  );
+
+  // Prepare the progress bar
+  let progressbarHandle = null;
+  const progressbarInterval = 1000;
+  const showProgress = () => {
+    readline.cursorTo(process.stdout, 0);
+    const toMB = (i) => (i / 1024 / 1024).toFixed(2);
+
+    process.stdout.write(
+      `Audio  | ${(
+        (tracker.audio.downloaded / tracker.audio.total) *
+        100
+      ).toFixed(2)}% processed `
+    );
+    process.stdout.write(
+      `(${toMB(tracker.audio.downloaded)}MB of ${toMB(
+        tracker.audio.total
+      )}MB).${" ".repeat(10)}\n`
+    );
+
+    process.stdout.write(
+      `Video  | ${(
+        (tracker.video.downloaded / tracker.video.total) *
+        100
+      ).toFixed(2)}% processed `
+    );
+    process.stdout.write(
+      `(${toMB(tracker.video.downloaded)}MB of ${toMB(
+        tracker.video.total
+      )}MB).${" ".repeat(10)}\n`
+    );
+
+    process.stdout.write(`Merged | processing frame ${tracker.merged.frame} `);
+    process.stdout.write(
+      `(at ${tracker.merged.fps} fps => ${tracker.merged.speed}).${" ".repeat(
+        10
+      )}\n`
+    );
+
+    process.stdout.write(
+      `running for: ${((Date.now() - tracker.start) / 1000 / 60).toFixed(
+        2
+      )} Minutes.`
+    );
+    readline.moveCursor(process.stdout, 0, -3);
+  };
+
+  // Start the ffmpeg child process
+  const ffmpegProcess = cp.spawn(
+    ffmpeg,
+    [
+      // Remove ffmpeg's console spamming
+      "-loglevel",
+      "8",
+      "-hide_banner",
+      // Redirect/Enable progress messages
+      "-progress",
+      "pipe:3",
+      // Set inputs
+      "-i",
+      "pipe:4",
+      "-i",
+      "pipe:5",
+      // Map audio & video from streams
+      "-map",
+      "0:a",
+      "-map",
+      "1:v",
+      // Keep encoding
+      "-c:v",
+      "copy",
+      // Define output file
+      "out.mkv",
+    ],
+    {
+      windowsHide: true,
+      stdio: [
+        /* Standard: stdin, stdout, stderr */
+        "inherit",
+        "inherit",
+        "inherit",
+        /* Custom: pipe:3, pipe:4, pipe:5 */
+        "pipe",
+        "pipe",
+        "pipe",
+      ],
+    }
+  );
+  ffmpegProcess.on("close", () => {
+    console.log("done");
+    // Cleanup
+    process.stdout.write("\n\n\n\n");
+    clearInterval(progressbarHandle);
+  });
+
+  // Link streams
+  // FFmpeg creates the transformer streams and we just have to insert / read data
+  ffmpegProcess.stdio[3].on("data", (chunk) => {
+    // Start the progress bar
+    if (!progressbarHandle)
+      progressbarHandle = setInterval(showProgress, progressbarInterval);
+    // Parse the param=value list returned by ffmpeg
+    const lines = chunk.toString().trim().split("\n");
+    const args = {};
+    for (const l of lines) {
+      const [key, value] = l.split("=");
+      args[key.trim()] = value.trim();
+    }
+    tracker.merged = args;
+  });
+  audio.pipe(ffmpegProcess.stdio[4]);
+  video.pipe(ffmpegProcess.stdio[5]);
 }
 
-function download(url, outputPath) {
+function convertMkvToMp4(mkvFilePath, mp4FilePath) {
   return new Promise((resolve, reject) => {
-    ytdl(url)
-      .pipe(fs.createWriteStream(outputPath))
-      .on("finish", () => {
-        console.log("Download completed.");
-        resolve();
-      })
-      .on("error", (error) => {
-        console.error("Error occurred:", error);
-        reject(error);
-      });
+    const ffmpegProcess = cp.spawn("ffmpeg", [
+      "-i",
+      mkvFilePath, // Input file
+      "-codec",
+      "copy",
+      mp4FilePath, // Output file
+    ]);
+
+    ffmpegProcess.on("close", (code) => {
+      if (code === 0) {
+        console.log(`Conversion complete: ${mp4FilePath}`);
+        resolve(mp4FilePath);
+      } else {
+        reject(new Error(`ffmpeg exited with code ${code}`));
+      }
+    });
+
+    ffmpegProcess.stderr.on("data", (data) => {
+      console.error(`ffmpeg stderr: ${data}`);
+    });
   });
 }
 
-async function downloadYoutubeVideo(channel, url, outputDirectory, videoID) {
-  try {
-    const info = await ytdl.getInfo(url);
-
-    const format = ytdl.chooseFormat(info.formats, {
-      quality: "highest",
-      filter: (format) => format.hasAudio && format.hasVideo,
-    });
-    console.log("Chosen Format!");
-    console.log(format);
-    console.log("Download URL: ", format.url);
-
-    const outputPath = path.join(
-      __dirname,
-      outputDirectory,
-      "videos",
-      videoID,
-      `${channel}_${videoID}.${format.container}`
-    );
-
-    await ensureDirectoryExists(outputPath);
-
-    await download(url, outputPath);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
 // Example usage
-// downloadYoutubeVideo(
-//   "minandjae",
-//   "https://www.youtube.com/shorts/NNeHrkNWyog",
-//   "../../resources/downloads/workout/youtube/whatmotivatedyou/shorts",
-//   "NNeHrkNWyog"
-// );
-
-ytdl("https://www.youtube.com/shorts/xiSUOlBVL7U", { quality: 18 }).pipe(
-  fs.createWriteStream("video.mp4")
-);
+convertMkvToMp4("out.mkv", "output.mp4")
+  .then(() => console.log("Conversion finished successfully"))
+  .catch((error) => console.error("Error:", error));
